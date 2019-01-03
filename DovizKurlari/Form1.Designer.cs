bd85688573a398b8c79e99bca658e352ce7a21e9
semvs <semvs@ayvansaray.edu.tr>
2019-01-03 14:22:03 +0300

    devam

---
+++ DovizKurlari/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace DovizKurlari
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblDovizKodu = new System.Windows.Forms.Label();
+            this.llbDovizAlis = new System.Windows.Forms.Label();
+            this.lblDovizSatis = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(27, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 199);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(48, 244);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(153, 228);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(559, 210);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // lblDovizKodu
+            // 
+            this.lblDovizKodu.AutoSize = true;
+            this.lblDovizKodu.Location = new System.Drawing.Point(29, 31);
+            this.lblDovizKodu.Name = "lblDovizKodu";
+            this.lblDovizKodu.Size = new System.Drawing.Size(59, 13);
+            this.lblDovizKodu.TabIndex = 4;
+            this.lblDovizKodu.Text = "DovizKodu";
+            // 
+            // llbDovizAlis
+            // 
+            this.llbDovizAlis.AutoSize = true;
+            this.llbDovizAlis.Location = new System.Drawing.Point(29, 89);
+            this.llbDovizAlis.Name = "llbDovizAlis";
+            this.llbDovizAlis.Size = new System.Drawing.Size(53, 13);
+            this.llbDovizAlis.TabIndex = 5;
+            this.llbDovizAlis.Text = "Doviz Aliş";
+            // 
+            // lblDovizSatis
+            // 
+            this.lblDovizSatis.AutoSize = true;
+            this.lblDovizSatis.Location = new System.Drawing.Point(29, 150);
+            this.lblDovizSatis.Name = "lblDovizSatis";
+            this.lblDovizSatis.Size = new System.Drawing.Size(60, 13);
+            this.lblDovizSatis.TabIndex = 6;
+            this.lblDovizSatis.Text = "Doviz Satis";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(126, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(126, 89);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(126, 150);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 9;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblDovizKodu);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.llbDovizAlis);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.lblDovizSatis);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(153, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(479, 199);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblDovizKodu;
+        private System.Windows.Forms.Label llbDovizAlis;
+        private System.Windows.Forms.Label lblDovizSatis;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ DovizKurlari/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace DovizKurlari
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.Load("http://www.tcmb.gov.tr/kurlar/201401/02012014.xml");
+            XmlElement rooteleman = xmldoc.DocumentElement;
+            XmlNodeList liste = rooteleman.GetElementsByTagName("Currency");
+            List<Doviz> dlist = new List<Doviz>();
+
+
+            foreach (XmlElement item in liste)
+            {
+                Doviz d = new Doviz();
+                XmlElement currency = item;
+                string code = currency.Attributes["CurrencyCode"].Value;
+
+                string isim = currency.GetElementsByTagName("Isim").Item(0).InnerText;
+
+                d.CurrencyName = isim;
+                string alisFiyat = currency.GetElementsByTagName("ForexBuying").Item(0).InnerText;
+                string satisFiyat = currency.GetElementsByTagName("ForexSelling").Item(0).InnerText;
+
+                if (!string.IsNullOrEmpty(alisFiyat))
+                {
+                    d.ForexBuying = Convert.ToDecimal(alisFiyat);
+                }
+                if (!string.IsNullOrEmpty(satisFiyat))
+                {
+                    d.ForexSelling = Convert.ToDecimal(satisFiyat);
+                }
+                if (!(code is null))
+                {
+                    d.CurrencyCode = code;
+                }
+                listBox1.Items.Add(d);
+                dlist.Add(d);
+            }
+            dataGridView1.DataSource = dlist;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Doviz secilenDoviz = (Doviz)listBox1.SelectedItem;
+            label5.Text = secilenDoviz.ForexBuying.ToString();
+            label6.Text = secilenDoviz.ForexSelling.ToString();
+            label4.Text = secilenDoviz.CurrencyName;
+        }
+    }
+}
